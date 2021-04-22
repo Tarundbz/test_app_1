9174a4ead9f621f23afdfa459db43bd75bc4a423
Badavath Tarun <tarunofman@gmail.com>
2021-04-23 04:08:51 +0530

    check android different scenarios

---
+++ app/src/main/java/com/example/android/dessertclicker/DessertTimer.kt
@@ -0,0 +1,36 @@
+//
+//package com.example.android.dessertclicker
+//
+//import android.os.Handler
+//import timber.log.Timber
+//import android.os.Looper
+
+//class DessertTimer {
+//
+//    
+//    var secondsCount = 0
+//
+//    private var handler = Handler(Looper.getMainLooper())
+//    private lateinit var runnable: Runnable
+//
+//
+//    fun startTimer() {
+//        
+//        runnable = Runnable {
+//            secondsCount++
+//            Timber.i("Timer is at : $secondsCount")
+//       
+//            handler.postDelayed(runnable, 1000)
+//        }
+//
+//       
+//        handler.postDelayed(runnable, 1000)
+//
+//        
+//    }
+//
+//    fun stopTimer() {
+//       
+//        handler.removeCallbacks(runnable)
+//    }
+//}
